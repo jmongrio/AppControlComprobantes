7c518a94f37c3b44f066a92e2eea420eb349b0ac
Jasón Mongrillo <jmongrio@hotmail.com>
2021-08-20 20:02:35 -0600

    Pantalla usuario administrador sin funcionalidad

---
+++ AppControlComprobantes/MVVM/View/PantallaUsuario.Designer.cs
@@ -0,0 +1,291 @@
+﻿
+namespace AppControlComprobantes.MVVM.View
+{
+    partial class btnAtras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(btnAtras));
+            this.lblBienvenida = new System.Windows.Forms.Label();
+            this.btnNuevoUsuario = new System.Windows.Forms.Button();
+            this.btnEditarUsuario = new System.Windows.Forms.Button();
+            this.btnEliminarUsuario = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnNuevoCliente = new System.Windows.Forms.Button();
+            this.btnEditarCliente = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnEliminarDeposito = new System.Windows.Forms.Button();
+            this.btnEditarDeposito = new System.Windows.Forms.Button();
+            this.btnNuevoDeposito = new System.Windows.Forms.Button();
+            this.btnEliminarCliente = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblBienvenida
+            // 
+            this.lblBienvenida.AutoSize = true;
+            this.lblBienvenida.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBienvenida.Location = new System.Drawing.Point(12, 9);
+            this.lblBienvenida.Name = "lblBienvenida";
+            this.lblBienvenida.Size = new System.Drawing.Size(374, 42);
+            this.lblBienvenida.TabIndex = 0;
+            this.lblBienvenida.Text = "Bienvenido, Nombre";
+            // 
+            // btnNuevoUsuario
+            // 
+            this.btnNuevoUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevoUsuario.Location = new System.Drawing.Point(12, 15);
+            this.btnNuevoUsuario.Name = "btnNuevoUsuario";
+            this.btnNuevoUsuario.Size = new System.Drawing.Size(200, 45);
+            this.btnNuevoUsuario.TabIndex = 1;
+            this.btnNuevoUsuario.Text = "Nuevo usuario";
+            this.btnNuevoUsuario.UseVisualStyleBackColor = true;
+            // 
+            // btnEditarUsuario
+            // 
+            this.btnEditarUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditarUsuario.Location = new System.Drawing.Point(12, 82);
+            this.btnEditarUsuario.Name = "btnEditarUsuario";
+            this.btnEditarUsuario.Size = new System.Drawing.Size(200, 45);
+            this.btnEditarUsuario.TabIndex = 2;
+            this.btnEditarUsuario.Text = "Editar usuario";
+            this.btnEditarUsuario.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarUsuario
+            // 
+            this.btnEliminarUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarUsuario.Location = new System.Drawing.Point(12, 149);
+            this.btnEliminarUsuario.Name = "btnEliminarUsuario";
+            this.btnEliminarUsuario.Size = new System.Drawing.Size(200, 45);
+            this.btnEliminarUsuario.TabIndex = 3;
+            this.btnEliminarUsuario.Text = "Eliminar usuario";
+            this.btnEliminarUsuario.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnNuevoUsuario);
+            this.panel1.Controls.Add(this.btnEliminarUsuario);
+            this.panel1.Controls.Add(this.btnEditarUsuario);
+            this.panel1.Location = new System.Drawing.Point(17, 138);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(227, 207);
+            this.panel1.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(12, 409);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(37, 29);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.btnEliminarCliente);
+            this.panel2.Controls.Add(this.btnEditarCliente);
+            this.panel2.Controls.Add(this.btnNuevoCliente);
+            this.panel2.Location = new System.Drawing.Point(283, 138);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(231, 207);
+            this.panel2.TabIndex = 6;
+            // 
+            // btnNuevoCliente
+            // 
+            this.btnNuevoCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevoCliente.Location = new System.Drawing.Point(15, 15);
+            this.btnNuevoCliente.Name = "btnNuevoCliente";
+            this.btnNuevoCliente.Size = new System.Drawing.Size(200, 45);
+            this.btnNuevoCliente.TabIndex = 2;
+            this.btnNuevoCliente.Text = "Nuevo cliente";
+            this.btnNuevoCliente.UseVisualStyleBackColor = true;
+            // 
+            // btnEditarCliente
+            // 
+            this.btnEditarCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditarCliente.Location = new System.Drawing.Point(15, 82);
+            this.btnEditarCliente.Name = "btnEditarCliente";
+            this.btnEditarCliente.Size = new System.Drawing.Size(200, 45);
+            this.btnEditarCliente.TabIndex = 3;
+            this.btnEditarCliente.Text = "Editar cliente";
+            this.btnEditarCliente.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.btnEliminarDeposito);
+            this.panel3.Controls.Add(this.btnEditarDeposito);
+            this.panel3.Controls.Add(this.btnNuevoDeposito);
+            this.panel3.Location = new System.Drawing.Point(555, 138);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(231, 207);
+            this.panel3.TabIndex = 7;
+            // 
+            // btnEliminarDeposito
+            // 
+            this.btnEliminarDeposito.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarDeposito.Location = new System.Drawing.Point(15, 149);
+            this.btnEliminarDeposito.Name = "btnEliminarDeposito";
+            this.btnEliminarDeposito.Size = new System.Drawing.Size(200, 45);
+            this.btnEliminarDeposito.TabIndex = 4;
+            this.btnEliminarDeposito.Text = "Elimianr depósito";
+            this.btnEliminarDeposito.UseVisualStyleBackColor = true;
+            // 
+            // btnEditarDeposito
+            // 
+            this.btnEditarDeposito.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditarDeposito.Location = new System.Drawing.Point(15, 82);
+            this.btnEditarDeposito.Name = "btnEditarDeposito";
+            this.btnEditarDeposito.Size = new System.Drawing.Size(200, 45);
+            this.btnEditarDeposito.TabIndex = 3;
+            this.btnEditarDeposito.Text = "Editar depósito";
+            this.btnEditarDeposito.UseVisualStyleBackColor = true;
+            // 
+            // btnNuevoDeposito
+            // 
+            this.btnNuevoDeposito.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevoDeposito.Location = new System.Drawing.Point(15, 15);
+            this.btnNuevoDeposito.Name = "btnNuevoDeposito";
+            this.btnNuevoDeposito.Size = new System.Drawing.Size(200, 45);
+            this.btnNuevoDeposito.TabIndex = 2;
+            this.btnNuevoDeposito.Text = "Nuevo depósito";
+            this.btnNuevoDeposito.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarCliente
+            // 
+            this.btnEliminarCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarCliente.Location = new System.Drawing.Point(15, 149);
+            this.btnEliminarCliente.Name = "btnEliminarCliente";
+            this.btnEliminarCliente.Size = new System.Drawing.Size(200, 45);
+            this.btnEliminarCliente.TabIndex = 4;
+            this.btnEliminarCliente.Text = "Eliminar cliente";
+            this.btnEliminarCliente.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(494, 409);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 29);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Panel de administración";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 48);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Administrador de \r\nusuarios";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(315, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 48);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Administrador de \r\nclientes";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(578, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(176, 48);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Comprobantes de\r\ndepósito";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAtras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblBienvenida);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "btnAtras";
+            this.Text = "Usuario";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBienvenida;
+        private System.Windows.Forms.Button btnNuevoUsuario;
+        private System.Windows.Forms.Button btnEditarUsuario;
+        private System.Windows.Forms.Button btnEliminarUsuario;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnNuevoCliente;
+        private System.Windows.Forms.Button btnEliminarCliente;
+        private System.Windows.Forms.Button btnEditarCliente;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnEliminarDeposito;
+        private System.Windows.Forms.Button btnEditarDeposito;
+        private System.Windows.Forms.Button btnNuevoDeposito;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
